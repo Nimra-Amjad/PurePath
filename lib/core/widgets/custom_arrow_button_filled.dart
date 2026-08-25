fbff5599e9ff7a09af48cd9248ec88077451558c
Nimra-Amjad <74602690+Nimra-Amjad@users.noreply.github.com>
2026-08-25 23:50:48 +0500

    update build no

--- lib/core/widgets/custom_arrow_button_filled.dart
+++ lib/core/widgets/custom_arrow_button_filled.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:purepath/core/constants/color_constants.dart';
+import 'package:purepath/core/utils/utils.dart';
 
 class CustomArrowButtonFilled extends StatelessWidget {
   final String svgAsset;
@@ -15,14 +17,16 @@ class CustomArrowButtonFilled extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: isDisabled ? null : onTap,
+    return CupertinoButton(
+      onPressed: isDisabled ? null : onTap,
+      padding: EdgeInsets.zero,
       child: Opacity(
         opacity: isDisabled ? 0.3 : 1.0,
-        child: CircleAvatar(
-          radius: 16,
-          backgroundColor: kWhiteColor,
-          child: SvgPicture.asset(svgAsset, width: 20, height: 20),
+        child: SvgPicture.asset(
+          svgAsset,
+          width: 20,
+          height: 20,
+          colorFilter: colorFilter(color: kWhiteColor),
         ),
       ),
     );
